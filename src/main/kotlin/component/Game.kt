341adface712eaf914896ec2ece838c8239b6b03
yt8492 <tmymyut2000@gmail.com>
2020-05-30 01:10:11 +0900

    refactor game object

--- src/main/kotlin/component/Game.kt
+++ src/main/kotlin/component/Game.kt
@@ -1,5 +1,6 @@
 package component
 
+import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.Image
 import org.w3c.dom.events.KeyboardEvent
 import react.RBuilder
@@ -10,25 +11,17 @@ import react.setState
 import kotlin.browser.window
 
 class Game : RComponent<RProps, Game.State>() {
-
     init {
         state.apply {
-            characterPosition = Position(600.0, 400.0)
+            player = GameObject(
+                    600.0,
+                    400.0,
+                    100.0,
+                    100.0,
+                    v3
+            )
             keydown = false
         }
-    }
-
-    private val v1 = Image().apply {
-        src = "./images/v1.svg"
-    }
-    private val v2 = Image().apply {
-        src = "./images/v2.svg"
-    }
-    private val v3 = Image().apply {
-        src = "./images/v3.svg"
-    }
-    private val v4 = Image().apply {
-        src = "./images/v4.svg"
     }
 
     private var keydown = false
@@ -53,17 +46,24 @@ class Game : RComponent<RProps, Game.State>() {
         window.requestAnimationFrame {
             updateStatus()
         }
-        if (keydown && state.characterPosition.y > 200) {
+        if (keydown && state.player.y > 200) {
             setState {
-                val y = state.characterPosition.y
-                characterPosition = state.characterPosition.copy(y = y - 1)
+                val y = player.y
+                player = player.copy(y = y - 3)
             }
-        } else if (!keydown && state.characterPosition.y < 400) {
+        } else if (!keydown && state.player.y < 400) {
             setState {
-                val y = state.characterPosition.y
-                characterPosition = state.characterPosition.copy(y = y + 1)
+                val y = player.y
+                player = player.copy(y = y + 3)
             }
         }
+    }
+
+    private fun drawGameObject(context: CanvasRenderingContext2D, gameObject: GameObject) {
+        gameObject.image.onload = {
+            context.drawImage(gameObject.image, gameObject.x, gameObject.y, gameObject.width, gameObject.height)
+        }
+        context.drawImage(gameObject.image, gameObject.x, gameObject.y, gameObject.width, gameObject.height)
     }
 
     override fun RBuilder.render() {
@@ -71,16 +71,34 @@ class Game : RComponent<RProps, Game.State>() {
         canvasComponent("800", "600") { context ->
             context.fillStyle = "#3C3C3C"
             context.fillRect(0.0, 0.0, 800.0, 600.0)
-            v3.onload = {
-                context.drawImage(v3, state.characterPosition.x, state.characterPosition.y, 100.0, 100.0)
-            }
-            context.drawImage(v3, state.characterPosition.x, state.characterPosition.y, 100.0, 100.0)
+            drawGameObject(context, state.player)
         }
     }
 
     interface State : RState {
-        var characterPosition: Position
+        var player: GameObject
     }
 
-    data class Position(val x: Double, val y: Double)
+    data class GameObject(
+            val x: Double,
+            val y: Double,
+            val width: Double,
+            val height: Double,
+            val image: Image
+    )
+
+    companion object {
+        private val v1 = Image().apply {
+            src = "./images/v1.svg"
+        }
+        private val v2 = Image().apply {
+            src = "./images/v2.svg"
+        }
+        private val v3 = Image().apply {
+            src = "./images/v3.svg"
+        }
+        private val v4 = Image().apply {
+            src = "./images/v4.svg"
+        }
+    }
 }
