ad7e513b3d7acdbc0dc8cbf5ea3e7d330a83de4a
yt8492 <tmymyut2000@gmail.com>
2020-05-31 04:33:46 +0900

    add enemies

--- src/main/kotlin/component/Game.kt
+++ src/main/kotlin/component/Game.kt
@@ -9,6 +9,8 @@ import react.RProps
 import react.RState
 import react.setState
 import kotlin.browser.window
+import kotlin.math.abs
+import kotlin.random.Random
 
 class Game : RComponent<RProps, Game.State>() {
 
@@ -23,7 +25,9 @@ class Game : RComponent<RProps, Game.State>() {
                             100.0,
                             100.0,
                             v3
-                    )
+                    ),
+                    emptyList(),
+                    0
             )
         }
         keydown = false
@@ -49,17 +53,20 @@ class Game : RComponent<RProps, Game.State>() {
         window.requestAnimationFrame {
             updateStatus()
         }
-        if (keydown && state.gameState !is GameState.Playing.PlayerJumping) {
+        val currentState = state.gameState
+        if (keydown && currentState is GameState.Playing.PlayerRunning) {
             keydown = false
             setState {
                 gameState = GameState.Playing.PlayerJumping(
-                        gameState.player,
-                        0
+                        currentState.player,
+                        currentState.enemyList,
+                        0,
+                        currentState.frame
                 )
             }
             return
         }
-        when (val currentState = state.gameState) {
+        when (currentState) {
             is GameState.Playing -> {
                 val nextState = currentState.calculateNextState()
                 if (currentState != nextState) {
@@ -83,13 +90,22 @@ class Game : RComponent<RProps, Game.State>() {
     }
 
     override fun RBuilder.render() {
-        println("render")
         canvasComponent(
                 state.gameState.canvasWidth.toString(),
                 state.gameState.canvasHeight.toString()
         ) { context ->
             context.fillStyle = "#3C3C3C"
             context.fillRect(0.0, 0.0, state.gameState.canvasWidth, state.gameState.canvasHeight)
+            when (val gameState = state.gameState) {
+                is GameState.Playing -> {
+                    gameState.enemyList.forEach {
+                        if (it.checkCollision(gameState.player)) {
+                            println("collision")
+                        }
+                        drawGameObject(context, it)
+                    }
+                }
+            }
             drawGameObject(context, state.gameState.player)
         }
     }
@@ -105,39 +121,83 @@ class Game : RComponent<RProps, Game.State>() {
             val height: Double,
             val image: Image,
             var imageLoaded: Boolean = false
-    )
+    ) {
+        fun checkCollision(other: GameObject): Boolean {
+            val centerX1 = this.x + this.width / 2
+            val centerX2 = other.x + other.width / 2
+            val centerY1 = this.y + this.height / 2
+            val centerY2 = other.y + other.height / 2
+            return abs(centerX1 - centerX2) < this.width / 2 + other.width / 2 &&
+                    abs(centerY1 - centerY2) < this.height / 2 + other.width / 2
+        }
+    }
 
     sealed class GameState {
         abstract val player: GameObject
+        abstract val frame: Int
         val canvasWidth = 800.0
         val canvasHeight = 600.0
         val groundY = 400.0
 
+        protected fun enemy(): GameObject {
+            return GameObject(
+                     0.0,
+                    groundY,
+                    100.0,
+                    100.0,
+                    v1,
+                    true
+            )
+        }
+
         sealed class Playing : GameState() {
             abstract fun calculateNextState(): GameState
+            abstract val enemyList: List<GameObject>
+
+            protected fun calculateNextEnemies(): List<GameObject> {
+                if (frame % listOf(100, 200).random() == 0) {
+                    return enemyList.asSequence()
+                            .map {
+                                it.copy(x = it.x + 8)
+                            }.filter {
+                                it.x < canvasWidth
+                            }.plus(enemy())
+                            .toList()
+                } else {
+                    return enemyList.asSequence()
+                            .map {
+                                it.copy(x = it.x + 8)
+                            }.filter {
+                                it.x < canvasWidth
+                            }.toList()
+                }
+            }
 
             data class PlayerJumping(
                     override val player: GameObject,
-                    private val t: Int
+                    override val enemyList: List<GameObject>,
+                    private val t: Int,
+                    override val frame: Int
             ) : Playing() {
                 override fun calculateNextState(): GameState {
                     val gravity = 0.4
-                    val vy = 10
+                    val vy = 13
                     val y = 0.5 * gravity * t * t - vy * t + (groundY)
-                    println("gravity: $gravity, t: $t, vy: $vy, playerY: ${player.y}")
                     return if (y <= groundY) {
-                        PlayerJumping(player.copy(y = y), t + 1)
+                        PlayerJumping(player.copy(y = y), calculateNextEnemies(), t + 1, frame + 1)
                     } else {
-                        PlayerRunning(player.copy(y = groundY))
+                        PlayerRunning(player.copy(y = groundY), calculateNextEnemies(), frame + 1)
                     }
                 }
             }
 
             data class PlayerRunning(
-                    override val player: GameObject
+                    override val player: GameObject,
+                    override val enemyList: List<GameObject>,
+                    override val frame: Int
             ) : Playing() {
                 override fun calculateNextState(): GameState {
-                    return this
+                    return this.copy(enemyList = calculateNextEnemies(), frame = frame + 1)
                 }
             }
         }
